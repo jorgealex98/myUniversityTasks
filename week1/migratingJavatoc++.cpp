13a8a8226ce009f6a69dc8631ef542f19ddb11c3
jorgealex98 <jorgealex2598@gmail.com>
2025-03-26 19:23:25 +0100

    a new class with three task comparing to java, need to complete the third class

---
+++ week1/migratingJavatoc++.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+
+
+void runMigratingToJava() {
+
+    // First problem(input your name)
+
+    /*std::cout << "Input  your name";
+
+    std::string name;
+    std::cin>> name ;
+
+    if (name == "Einstein") {
+        std::cout << "I know you!";
+    } else {
+        std::cout << "Greetings";
+    }*/
+
+    // Second problem(guess the number)
+
+    /*// Step 1: Create a random device
+    std::random_device rd;
+
+    // Step 2: Create a Mersenne Twister generator and seed it with random_device
+    std::mt19937 gen(rd());
+
+    // Step 3: Define the distribution range (for example, numbers from 1 to 100)
+    std::uniform_int_distribution<> distrib(1, 10);
+
+    // Step 4: Generate random numbers
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Random Number: " << distrib(gen) << std::endl;
+    }*/
+
+    /*std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, 100);
+
+    auto numberToGuess = distrib(gen);
+    auto input = -1;
+
+
+    while (input != numberToGuess) {
+
+        std::cout << "Guess a number " << '\n';
+        std::cin >> input;
+
+
+        if (input > numberToGuess) {
+            std::cout << "Too big. Try Lower" << '\n';
+        }
+        if ( input < numberToGuess) {
+            std::cout << "Too small. Try Higher" << '\n';
+        }
+
+    }
+
+
+    std::cout << "Congratulation! You guessed the number" << '\n';
+    */
+
+    // third problem
+
+    auto numbers = std::vector<int>{};
+
+    int input;
+
+    while (std::cin >> input && input >= 0  ) {
+        numbers.push_back(input);
+
+
+    }
+
+
+
+
+
+}
+
+
